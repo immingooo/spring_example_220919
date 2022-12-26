c1ffc0bfacc5b432675f0987c1fa7dcb70da099f
test <test@test.com>
2022-12-26 18:43:58 +0900

    Spring INSERT문 다루기 예제

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -15,4 +15,16 @@ public class ReviewBO {
 	public Review getReview(int id) { // BO메소드 호출할 때 id를 필수로 보내야겠구나.(Integer은 null도 가능하구나) BO에 입장에서 null이 안들어가면 int / null허용은 Integer
 		return reviewDAO.selectReview(id);
 	}
+	
+	// Insert
+	// input: Review 객체(하나의 행을 insert 해달라)
+	// output: int(insert가 성공된 행 수) => Mybatis가 알고있음
+	public int addReview(Review review) { // BO가 컨트롤러한테 안줄수도 있음
+		return reviewDAO.insertReview(review);
+	}
+	
+	// addReviewAsField(4, "콤비네이션R", "신비", 5.0, "역시 JMT")
+	public int addReviewAsField(int storeId, String menu, String userName, Double point, String review) { // 필드명하고 일치하게 짓기
+		return reviewDAO.insertReviewAsField(storeId, menu, userName, point, review); // 자동완성 믿지말기. 순서가 다를수도 있음
+	}
 }
--- src/main/java/com/example/lesson03/dao/ReviewDAO.java
+++ src/main/java/com/example/lesson03/dao/ReviewDAO.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.model.Review;
@@ -8,4 +9,14 @@ import com.example.lesson03.model.Review;
 public interface ReviewDAO {
 
 	public Review selectReview(int id); // BO거 복붙해서 수정해서 쓰기. 한 개면 @Param("") 이거 생략가능
+	
+	public int insertReview(Review review); // 행의 갯수를 (Mybatis가 자동으로)int로 return. 넘겨주는 값이 하나면 굳이 @Param하지말기
+	
+	public int insertReviewAsField(
+			// 하나의 맵으로 구성됨(@Param)
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu, 
+			@Param("userName") String userName,
+			@Param("point") Double point, 
+			@Param("review") String review);
 }
