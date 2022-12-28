2afe872414109868e5c1b0abf7d1faf24f8d06db
ming <ming@home.com>
2022-12-28 21:46:58 +0900

    Spring JSP 예제 (DB Insert 과제 제출)

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -1,16 +1,21 @@
 package com.example.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.lesson04.bo.StudentBO;
 import com.example.lesson04.model.Student;
 
 @Controller
 @RequestMapping("/lesson04/ex02")
 public class Lesson04Ex02Controller {
+	
+	@Autowired
+	private StudentBO studentBO;
 
 	// 학생 추가 화면
 	// http://localhost/lesson04/ex02/add_student_view
@@ -24,7 +29,8 @@ public class Lesson04Ex02Controller {
 			@ModelAttribute Student student // name(request Param)과 필드명이 일치하는 것이 담긴다.
 			) { // @ModelAttribute 생략가능(여러개 객체를 받을 땐 붙여야 함)
 		
-		// DB Insert - student객체 넘겨서 이 줄만 과제제출하기 
+		// DB Insert
+		studentBO.addStudent(student);
 		
 		// DB select(방금 가입된 사람)
 		
---
+++ src/main/java/com/example/lesson04/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.example.lesson04.dao;
+
+import com.example.lesson04.model.Student;
+
+public interface StudentDAO {
+
+	public void insertStudent(Student student);
+}
