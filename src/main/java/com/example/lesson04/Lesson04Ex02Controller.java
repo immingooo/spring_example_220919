3d216411efcf0a6c39d0507bb93a0332ffc3c898
test <test@test.com>
2022-12-28 18:41:51 +0900

    Spring JSP 예제 이어서

---
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -0,0 +1,34 @@
+package com.example.lesson04;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.lesson04.model.Student;
+
+@Controller
+@RequestMapping("/lesson04/ex02")
+public class Lesson04Ex02Controller {
+
+	// 학생 추가 화면
+	// http://localhost/lesson04/ex02/add_student_view
+	@GetMapping("/add_student_view")
+	public String addStudentView() {
+		return "lesson04/addStudent";
+	}
+	
+	@PostMapping("/add_student")
+	public String addStudent( // 파라미터 값이 들어왔는지랑 응답값 이동하는 거 확인하기
+			@ModelAttribute Student student // name(request Param)과 필드명이 일치하는 것이 담긴다.
+			) { // @ModelAttribute 생략가능(여러개 객체를 받을 땐 붙여야 함)
+		
+		// DB Insert - student객체 넘겨서 이 줄만 과제제출하기 
+		
+		// DB select(방금 가입된 사람)
+		
+		// view(jsp) 페이지 응답값(이동)
+		return "lesson04/afterAddStudent";
+	}
+}
